9c173b2ecefe3eeaf3857cb6300e12c76559afa3
Leandro Leite <ndronos@gmail.com>
2016-12-06 00:26:23 -0200

    Create hello app from section 01

---
+++ hello-mac-xcode.c
@@ -0,0 +1,16 @@
+//
+// main.c
+// hello
+//
+// from Learn C as Your First Programming Language
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+ 
